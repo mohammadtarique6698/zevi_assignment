326f98d83f9a82a2b6a0ca556d090d731ca5d885
mohammadtarique6698 <mohammadtarique661998@gmail.com>
2023-12-29 10:18:42 +0530

    Final Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Front from "./Components/1stPage/Front_Page/FrontPage";
 import Page2nd from "./Components/2ndPage/MainPage/MainPage";
@@ -15,24 +14,7 @@ const App: React.FC = () => {
         </Routes>
       </div>
     </Router>
-=======
-//import Front from "./Components/1stPage/Front_Page/FrontPage";
-import Page2nd from "./Components/2ndPage/MainPage/MainPage"
-import "./App.css";
-
-
-const App: React.FC =() => {
-  return (
-    <div className="App">
-        {/* <Front color="White" /> */}
-        <Page2nd color="Black" />
-    </div>
->>>>>>> 3a9bd95aeaa6b908b0131a242b9c448e4818602e
   );
 };
 
 export default App;
-<<<<<<< HEAD
-=======
-
->>>>>>> 3a9bd95aeaa6b908b0131a242b9c448e4818602e
